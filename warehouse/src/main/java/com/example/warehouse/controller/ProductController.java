912be359ebdbced3844619f72d4ce94b8fc6fcfb
Manikanta Satya Subbareddy Kovvuri <subbareddy934@gmail.com>
2024-09-22 16:13:53 +0530

    Solution with all test case pass

--- warehouse/src/main/java/com/example/warehouse/controller/ProductController.java
+++ warehouse/src/main/java/com/example/warehouse/controller/ProductController.java
@@ -10,42 +10,42 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ProductController {
 
-  /*
+	/*
  This controller would be responsible for the ProductController endpoints
  Add the required annotations and create the endpoints
-  */
+	 */
 
 	@Autowired
-  private ProductService productService;
+	private ProductService productService;
 
-  //to add the Product details using Product object
-  @PostMapping("/product/add")
-  private Object postProduct(@RequestBody Product product){
-    return productService.postProduct(product);
-  }
+	//to add the Product details using Product object
+	@PostMapping("/product/add")
+	private Object postProduct(@RequestBody Product product){
+		return productService.postProduct(product);
+	}
 
-  //to get all the Product details
-  @GetMapping("/product/get")
-  private Object getProduct(){
-    return productService.getProduct();
-  }
+	//to get all the Product details
+	@GetMapping("/product/get")
+	private Object getProduct(){
+		return productService.getProduct();
+	}
 
 
-  //to update the product with id as pathVariable and product as object in RequestBody
-  @PutMapping("/product/{id}")
-  public ResponseEntity<Object> updateProduct(@PathVariable int id,@RequestBody Product product){
-    return productService.updateProduct(id, product);
-  }
+	//to update the product with id as pathVariable and product as object in RequestBody
+	@PutMapping("/product/{id}")
+	public ResponseEntity<Object> updateProduct(@PathVariable int id,@RequestBody Product product){
+		return productService.updateProduct(id, product);
+	}
 
-  // to delete the product by using id as PathVariable
-  @DeleteMapping("/product/{id}")
-  public ResponseEntity<Object> deleteProductById(@PathVariable int id){
-    return productService.deleteProductById(id);
-  }
+	// to delete the product by using id as PathVariable
+	@DeleteMapping("/product/{id}")
+	public ResponseEntity<Object> deleteProductById(@PathVariable int id){
+		return productService.deleteProductById(id);
+	}
 
-  //to get the product items by vendor
-  @GetMapping("/product/vendor")
-  public ResponseEntity<Object> getSimilarVendor(@PathVariable String value){
-    return productService.getSimilarVendor(value);
-  }
+	//to get the product items by vendor
+	@GetMapping("/product/vendor")
+	public ResponseEntity<Object> getSimilarVendor(@RequestParam String value){
+		return productService.getSimilarVendor(value);
+	}
 }
--- warehouse/src/main/java/com/example/warehouse/service/ProductService.java
+++ warehouse/src/main/java/com/example/warehouse/service/ProductService.java
@@ -16,90 +16,94 @@ import java.util.Optional;
 @Service
 public class ProductService {
 
-  /*
+	/*
   Implement the business logic for the ProductService operations in this class
   Make sure to add required annotations
-  */
+	 */
 
-  @Autowired
-  private ProductRepository productRepository;
-
-
-  //to post all the Product details
-  //created->201
-  //badRequest->400
-  public Object postProduct(Product product) throws DataIntegrityViolationException{
-  	Product p = productRepository.findBySku(product.getSku());
-  	if(p==null) {
-  		Product p1 = productRepository.saveAndFlush(product);
-  		if(p1!=null) {
-  			return new ResponseEntity<>(p1,HttpStatus.CREATED);
-  		}
-  		
-  	}
-    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-  }
-
-  //to get all the Product details
-  //ok->200
-  //badRequest->400
-  public Object getProduct() throws DataIntegrityViolationException{
-  	List<Product> pl= null;
-  	pl=productRepository.findAll();
-  	if(pl!=null && !pl.isEmpty()) {
-  		new ResponseEntity<>(pl,HttpStatus.OK);
-  	}
-    return new ResponseEntity<>(pl,HttpStatus.BAD_REQUEST);
-  }
-
-  //to get the product with the value(pathVariable)
-  //ok()->200
-  //badRequest()->400
-  public ResponseEntity<Object> getSimilarVendor(String value) throws DataIntegrityViolationException{
-  	List<Product> p = productRepository.findByVendor(value);
-  	if(p!=null) {
-  		return new ResponseEntity<>(p,HttpStatus.OK);
-  	}
-    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-  }
+	@Autowired
+	private ProductRepository productRepository;
 
 
-  //to update the Product with id as pathVariable and Product as object in RequestBody
-  //ok->200
-  //badRequest->400
-  public ResponseEntity<Object> updateProduct(int id, Product product) throws DataIntegrityViolationException{
-  	Optional<Product> p = productRepository.findById(id);
-  	if(p.isPresent()) {
-  		Product p1 = p.get();
-  		Product p2 = productRepository.findBySku(p1.getSku());
-  		if(p2==null) {
-	  		p1.setCurrency(product.getCurrency());
-	  		p1.setDescription(product.getDescription());
-	  		p1.setImage_url(product.getImage_url());
-	  		p1.setName(product.getName());
-	  		p1.setPrice(product.getPrice());
-	  		p1.setStock(product.getStock());
-	  		p1.setVendor(product.getVendor());
-	  		productRepository.saveAndFlush(p1);
-	  		return new ResponseEntity<>(HttpStatus.OK);
-	  		
-  		}
-  		
-  	}
-    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-  }
+	//to post all the Product details
+	//created->201
+	//badRequest->400
+	public Object postProduct(Product product) throws DataIntegrityViolationException{
+		Product p = productRepository.findBySku(product.getSku());
+		if(p==null) {
+			Product p1 = productRepository.saveAndFlush(product);
+			if(p1!=null) {
+				return new ResponseEntity<>(p1,HttpStatus.CREATED);
+			}
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
 
-  // to delete the product by using id as PathVariable
-  //ok->200
-  //badRequest->400
-  public ResponseEntity<Object> deleteProductById(int id) throws DataIntegrityViolationException{
-  	Optional<Product> p = productRepository.findById(id);
-  	if(p.isPresent() && p!=null) {
-  		productRepository.delete(p.get());
-  		new ResponseEntity<>(HttpStatus.OK);
-  	}
-    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-  }
+	//to get all the Product details
+	//ok->200
+	//badRequest->400
+	public Object getProduct() throws DataIntegrityViolationException{
+		List<Product> pl = productRepository.findAll();
+		if (pl != null && !pl.isEmpty()) {
+			return new ResponseEntity<>(pl, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+	//to get the product with the value(pathVariable)
+	//ok()->200
+	//badRequest()->400
+	public ResponseEntity<Object> getSimilarVendor(String value) throws DataIntegrityViolationException{
+		List<Product> p = productRepository.findByVendor(value);
+		if(p!=null && !p.isEmpty()) {
+			return new ResponseEntity<>(p,HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+
+	//to update the Product with id as pathVariable and Product as object in RequestBody
+	//ok->200
+	//badRequest->400
+	public ResponseEntity<Object> updateProduct(int id, Product product) throws DataIntegrityViolationException{
+		Optional<Product> p = productRepository.findById(id);
+		if(p.isPresent()) {
+			Product p1 = p.get();
+
+			if(product.getCurrency()!=null) {
+				p1.setCurrency(product.getCurrency());
+			}
+			if(product.getDescription()!=null) {
+				p1.setDescription(product.getDescription());
+			}
+			if(product.getImage_url()!=null) {
+				p1.setImage_url(product.getImage_url());
+			}
+			if(product.getName()!=null) {
+				p1.setName(product.getName());
+			}
+			p1.setPrice(product.getPrice());
+			p1.setStock(product.getStock());
+			if(product.getVendor()!=null) {
+				p1.setVendor(product.getVendor());
+			}
+			productRepository.saveAndFlush(p1);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+	// to delete the product by using id as PathVariable
+	//ok->200
+	//badRequest->400
+	public ResponseEntity<Object> deleteProductById(int id) throws DataIntegrityViolationException{
+		Optional<Product> p = productRepository.findById(id);
+		if(p.isPresent()) {
+			productRepository.delete(p.get());
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
 
 
 
